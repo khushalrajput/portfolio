fbc0e1f195f05d0aaea339c2655f91e336d68db5
khushalrajput <rajputkhushal31@gmail.com>
2026-08-21 21:16:22 +0530

    feat: set up VS Code shell layout structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarService } from './core/services/sidebar.service';
+import { TitleBarComponent } from './shell/title-bar/title-bar.component';
+import { MenuBarComponent } from './shell/menu-bar/menu-bar.component';
+import { ActivityBarComponent } from './shell/activity-bar/activity-bar.component';
+import { SidebarComponent } from './shell/sidebar/sidebar.component';
+import { TabBarComponent } from './shell/tab-bar/tab-bar.component';
+import { BreadcrumbComponent } from './shell/breadcrumb/breadcrumb.component';
+import { TerminalPaneComponent } from './shell/terminal-pane/terminal-pane.component';
+import { StatusBarComponent } from './shell/status-bar/status-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TitleBarComponent,
+    MenuBarComponent,
+    ActivityBarComponent,
+    SidebarComponent,
+    TabBarComponent,
+    BreadcrumbComponent,
+    TerminalPaneComponent,
+    StatusBarComponent,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('portfolio');
+  constructor(protected readonly sidebarService: SidebarService) {}
 }
